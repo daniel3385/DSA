f6c72eca75f42e8e308545ab0983b1258f2b22e8
daniel <daniel.cabral88@gmail.com>
2018-06-16 14:10:52 -0300

    separate in folders

--- genericSort.h
+++
@@ -1,8 +0,0 @@
-/*
-  insert sort 
-  sort arr, using of size n using compare function
-*/
-
-void insertSort(void *, int, int,  int (*)(const void *, const void *)); 
-  
-  
---
+++ sort/genericSort.h
@@ -0,0 +1,8 @@
+/*
+  insert sort 
+  sort arr, using of size n using compare function
+*/
+
+void insertSort(void *arr, int numberElements, int sizeElement, int (*compare)(const void *a, const void *b));
+  
+  
